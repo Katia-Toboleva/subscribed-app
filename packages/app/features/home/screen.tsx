5d045e839b06ca0b4e5e45ed78a8288ee87d7fe5
Katia <katia.toboleva@and.digital>
2022-06-24 17:17:48 +0100

    updates

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -13,18 +13,13 @@ export function HomeScreen() {
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
       <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
+        <H1 ta="center">Welcome to Subscribed.</H1>
         <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This
-          screen uses the same code on Next.js and React Native.
+          I will be a great APP some time soon!
         </Paragraph>
         <Separator />
         <Paragraph ta="center">
-          Tamagui is made by{' '}
-          <Anchor href="https://twitter.com/natebirdman" target="_blank">
-            Nate Wienert
-          </Anchor>,
-          give it a star <Anchor href="https://github.com/tamagui/tamagui" target="_blank" rel="noreferrer">on Github</Anchor>.
+          This is an app that helps you keep track of your subscriptions
         </Paragraph>
       </YStack>
 
